bc10c636edd3d5d5e05a581b25785ebef44b9696
roff-dev <kieronoates@hotmail.co.uk>
2025-03-20 10:21:20 +0000

    added comic book controller

---
+++ src/ComicBookGallery/Controllers/ComicBooksController.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using ComicBookGallery.Models;
+
+namespace ComicBookGallery.Controllers
+{
+    public class ComicBooksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
